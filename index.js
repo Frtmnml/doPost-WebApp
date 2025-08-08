49fad1991ad0eecfcecbcf4fa19c5d45f00d9e7d
Joshua Herrera <42598687+Frtmnml@users.noreply.github.com>
2025-08-07 18:44:50 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-fetch("https://script.google.com/macros/s/AKfycbxdTuulBuJ2_SQY184JhQBGjybaoLLKf6GuAy0fAos1Qtvyiw7zLUFiE59KXUEjvek/exec")
+fetch("https://script.google.com/macros/s/AKfycbx4thpONC6C4osKA9rSzEXWnvxBxWGsMVt2gy01hha62dMuY6ZLMuMWwBOzSjNHRuRc/exec")
   .then(res => res.json())
   .then(data => {
     console.log("Datos recibidos:", data);
